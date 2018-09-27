3b5b260753442f6198816c1ce1d6c2f9e6df2cb8
feynman <gs2dang07@gmail.com>
2018-09-27 15:27:21 +0900

    blog app 및 Post Model추가, migration생성

---
+++ app/blog/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.utils import timezone
+
+
+# Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE, )
+    title = models.CharField(max_length=200)
+    text = models.TextField(blank=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    # repr 역할도 같이 한다.
+    def __str__(self):
+        return self.title
